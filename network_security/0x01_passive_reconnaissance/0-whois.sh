2d1e4eef5e717a61498f231fc9bdfce8da09b4b9
root <root@mrnobody.mrnobody>
2025-12-27 14:47:58 +0300

    dasH

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F': ' '/^(Admin|Tech|Registrant)/ {print $0}' > "$1".csv
+whois "$1" | awk -F: '/^Registrant|^Admin|^Tech/ {if ($1 ~ /Ext/) $1=$1":"; if ($2=="") $2=" "; print $1 "," $2}' > "${1}.csv"
