40955d2eab7c16e055b400db03c511fa07efc0f5
root <root@mrnobody.mrnobody>
2026-02-05 12:36:39 +0300

    dasH

--- cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
+++ cybersecurity_basics/0x03_cryptography_basics/3-password_hash.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-salt=$(openssl rand -hex 8)
-password="$salt$1"
-echo -n  "$password" | openssl dgst -sha512 > 3_hash.txt
+echo -n "$1$(openssl rand -hex 8)" | openssl dgst -sha512 | awk '{print $2}' > 3_hash.txt
