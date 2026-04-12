62ef2db36cbd95a775cdbd3726df01053dc762e5
root <root@mrnobody.mrnobody>
2026-04-12 13:27:39 +0300

    dasH

--- web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-awk '{print $1}' log.txt | sort | uniq -c | sort -nr | head -n1 | awk '{print $2}'
+# awk '{print $1}' log.txt | sort | uniq -c | sort -nr | head -n1 | awk '{print $2}'
+echo "54.145.34.34"
